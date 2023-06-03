f1c2044093c7d4e7a05e8982876eec70d96659d6
hamedakbarii <hamedkarate0@gmail.com>
2023-06-03 20:28:55 +0430

    project pages created

---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+import Index from "./Pages/Index";
+import CourseInfo from "./Pages/CourseInfo";
+import Category from "./Pages/Category";
+import ArticleInfo from "./Pages/ArticleInfo";
+
+const routes = [
+  { path: "/", element: <Index /> },
+  { path: "/course-info/:courseName", element: <CourseInfo /> },
+  { path: "/category-info/:categoryName", element: <Category /> },
+  { path: "/article-info/:articleName", element: <ArticleInfo /> },
+];
+
+export default routes;
