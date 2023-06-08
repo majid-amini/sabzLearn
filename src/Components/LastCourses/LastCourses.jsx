9200b88708636cc05040c3d19c9e7cf96c951e43
hamedakbarii <hamedkarate0@gmail.com>
2023-06-08 16:04:00 +0430

    courseBox section added

--- src/Components/LastCourses/LastCourses.jsx
+++ src/Components/LastCourses/LastCourses.jsx
@@ -1,19 +1,34 @@
 import React from "react";
-import "./LastCourses.css";
+import CourseBox from "../CourseBox/CourseBox";
 import SectionHeader from "../SectionHeader/SectionHeader";
+
+import "./LastCourses.css";
 
 export default function LastCourses() {
   return (
-    <div>
+    <>
       <div class="courses">
         <div class="container">
           <SectionHeader
-            title={"جدیدترین دوره ها"}
-            desc={"سکوی پرتاب شما به سمت موفقیت"}
-            btnTitle={"تمامی دوره ها "}
+            title="جدیدترین دوره ها"
+            desc="سکوی پرتاپ شما به سمت موفقیت"
+            btnTitle="تمامی دوره ها"
           />
+
+          <div class="courses-content">
+            <div class="container">
+              <div class="row">
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
