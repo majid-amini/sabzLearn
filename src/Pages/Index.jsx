702768d0a1e1df426a5aadde0008b94cdf73c01a
hamedakbarii <hamedkarate0@gmail.com>
2023-06-19 02:45:09 +0330

    about us section added to the project

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Components/Header/Header";
 import LastCourses from "../Components/LastCourses/LastCourses";
+import AboutUs from "../Components/AboutUs/AboutUs";
 
 export default function Index() {
   return (
     <div>
       <Header />
       <LastCourses />
+      <AboutUs />
     </div>
   );
 }
