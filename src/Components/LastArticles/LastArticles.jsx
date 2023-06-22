99b53a19a605e09afcd721be20b17b5f4b83c8e8
hamedakbarii <hamedkarate0@gmail.com>
2023-06-22 15:00:29 +0330

    articles box section added to the project

--- src/Components/LastArticles/LastArticles.jsx
+++ src/Components/LastArticles/LastArticles.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./LastArticles.css";
 import SectionHeader from "../SectionHeader/SectionHeader";
+import ArticleBox from "../ArticleBox/ArticleBox";
 
 export default function LastArticles() {
   return (
@@ -11,6 +12,38 @@ export default function LastArticles() {
           desc={"پیش به سوی ارتقای دانش"}
           btnTitle={"تمامی دوره ها"}
         />
+
+        <div className="articles__content">
+          <div className="row">
+            <ArticleBox
+              title={
+                "نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه در پایتون "
+              }
+              cover={"images/blog/3.jpg"}
+              desc={
+                "زبان پایتون هم مانند دیگر زبان های برنامه نویسی رایج ، دارای کتبخانه های مختلفی برای تسریع ..."
+              }
+            />
+            <ArticleBox
+              title={
+                "نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه در پایتون "
+              }
+              cover={"images/blog/3.jpg"}
+              desc={
+                "زبان پایتون هم مانند دیگر زبان های برنامه نویسی رایج ، دارای کتبخانه های مختلفی برای تسریع ..."
+              }
+            />
+            <ArticleBox
+              title={
+                "نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه در پایتون "
+              }
+              cover={"images/blog/3.jpg"}
+              desc={
+                "زبان پایتون هم مانند دیگر زبان های برنامه نویسی رایج ، دارای کتبخانه های مختلفی برای تسریع ..."
+              }
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
