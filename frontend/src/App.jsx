92cd8d542b80da6063f7c59d9ff4992c37506084
majid-amini <majidamini_s@yahoo.com>
2023-08-03 11:17:27 +0330

    authContex file and it's properties such as createContex and login properties added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import routes from "./routes";
 import { useRoutes } from "react-router-dom";
+import AuthContext from "./contex/authcontex";
 
 export default function App() {
   const router = useRoutes(routes);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(false);
+  const [userInfos, setUserInfos] = {};
 
-  return <div>{router}</div>;
+  const login = (token) => {
+    // setIsLoggedIn(true)
+    setToken(token);
+    localStorage.setItem("user", JSON.stringify({ token }));
+  };
+
+  const logout = () => {
+    setToken(null);
+    setUserInfos({})
+    localStorage.removeItem("user");
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        token,
+        userInfos,
+        login,
+        logout,
+      }}
+    >
+      {router}
+    </AuthContext.Provider>
+  );
 }
