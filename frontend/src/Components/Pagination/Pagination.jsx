0f20d250b8bdf93ec542da048781652dea5180c4
majid-amini <majidamini_s@yahoo.com>
2023-08-19 15:25:52 +0330

    active class for pagination added with conditional rendering

--- frontend/src/Components/Pagination/Pagination.jsx
+++ frontend/src/Components/Pagination/Pagination.jsx
@@ -9,7 +9,7 @@ export default function Pagination({
   pathName,
   setShowCourses,
 }) {
-  const [pageCount, setPageCount] = useState(null);
+  const [pagesCount, setPagesCount] = useState(null);
   const { page } = useParams();
 
   useEffect(() => {
@@ -19,18 +19,35 @@ export default function Pagination({
     setShowCourses(paginatedItems);
 
     let pagesNumber = Math.ceil(items.length / itemCount);
-    setPageCount(pagesNumber);
+    setPagesCount(pagesNumber);
   }, [page, items]);
   // console.log(itemCount);
   return (
     <div className="courses-pagination">
       <ul className="courses__pagination-list">
-        <li className="courses__pagination-item">
-          <Link to={""} className="courses__pagination-link">
-            <i className="fas fa-long-arrow-alt-right courses__pagination-icon"></i>
-          </Link>
-        </li>
-        <li className="courses__pagination-item">
+        {Array(pagesCount)
+          .fill(0)
+          .map((item, index) => (
+            <li className="courses__pagination-item">
+              {index + 1 === Number(page) ? (
+                <Link
+                  to={`${pathName}/${index + 1}`}
+                  className="courses__pagination-link courses__pagination-link--active"
+                >
+                  {index + 1}
+                </Link>
+              ) : (
+                <Link
+                  to={`${pathName}/${index + 1}`}
+                  className="courses__pagination-link"
+                >
+                  {index + 1}
+                </Link>
+              )}
+            </li>
+          ))}
+
+        {/* <li className="courses__pagination-item">
           <Link to={""} className="courses__pagination-link">
             1
           </Link>
@@ -52,7 +69,7 @@ export default function Pagination({
           <Link to={""} className="courses__pagination-link">
             <i className="fas fa-long-arrow-alt-left courses__pagination-icon"></i>
           </Link>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
