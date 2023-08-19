e631f0d9fd3acb48c5ba8fd2c61707b45bffd032
majid-amini <majidamini_s@yahoo.com>
2023-08-19 09:43:49 +0330

    conditional rendering added to category section for showing an alert when there is no category

--- frontend/src/Pages/Category/Category.jsx
+++ frontend/src/Pages/Category/Category.jsx
@@ -8,7 +8,6 @@ import Pagination from "../../Components/Pagination/Pagination";
 import { useParams } from "react-router-dom";
 
 export default function Category() {
-  
   const [courses, setCourses] = useState([]);
   const { categoryName } = useParams();
 
@@ -27,66 +26,72 @@ export default function Category() {
 
       <section className="courses">
         <div className="container">
-          <div className="courses-top-bar">
-            <div className="courses-top-bar__right">
-              <div className="courses-top-bar__row-btn courses-top-bar__icon--active">
-                <i className="fas fa-border-all courses-top-bar__icon"></i>
-              </div>
-              <div className="courses-top-bar__column-btn">
-                <i className="fas fa-align-left courses-top-bar__icon"></i>
-              </div>
-
-              <div className="courses-top-bar__selection">
-                <span className="courses-top-bar__selection-title">
-                  مرتب سازی پیش فرض
-                  <i className="fas fa-angle-down courses-top-bar__selection-icon"></i>
-                </span>
-                <ul className="courses-top-bar__selection-list">
-                  <li className="courses-top-bar__selection-item courses-top-bar__selection-item--active">
-                    مرتب سازی پیش فرض
-                  </li>
-                  <li className="courses-top-bar__selection-item">
-                    مربت سازی بر اساس محبوبیت
-                  </li>
-                  <li className="courses-top-bar__selection-item">
-                    مربت سازی بر اساس امتیاز
-                  </li>
-                  <li className="courses-top-bar__selection-item">
-                    مربت سازی بر اساس آخرین
-                  </li>
-                  <li className="courses-top-bar__selection-item">
-                    مربت سازی بر اساس ارزان ترین
-                  </li>
-                  <li className="courses-top-bar__selection-item">
-                    مربت سازی بر اساس گران ترین
-                  </li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="courses-top-bar__left">
-              <form action="#" className="courses-top-bar__form">
-                <input
-                  type="text"
-                  className="courses-top-bar__input"
-                  placeholder="جستجوی دوره ..."
-                />
-                <i className="fas fa-search courses-top-bar__search-icon"></i>
-              </form>
-            </div>
-          </div>
-
           <div className="courses-content">
             <div className="container">
               <div className="row">
-                {courses.map((course) => (
-                  <CourseBox {...course}/>
-                ))}
+                {courses.length !== 0 ? (
+                  <>
+                    <div className="courses-top-bar">
+                      <div className="courses-top-bar__right">
+                        <div className="courses-top-bar__row-btn courses-top-bar__icon--active">
+                          <i className="fas fa-border-all courses-top-bar__icon"></i>
+                        </div>
+                        <div className="courses-top-bar__column-btn">
+                          <i className="fas fa-align-left courses-top-bar__icon"></i>
+                        </div>
+
+                        <div className="courses-top-bar__selection">
+                          <span className="courses-top-bar__selection-title">
+                            مرتب سازی پیش فرض
+                            <i className="fas fa-angle-down courses-top-bar__selection-icon"></i>
+                          </span>
+                          <ul className="courses-top-bar__selection-list">
+                            <li className="courses-top-bar__selection-item courses-top-bar__selection-item--active">
+                              مرتب سازی پیش فرض
+                            </li>
+                            <li className="courses-top-bar__selection-item">
+                              مربت سازی بر اساس محبوبیت
+                            </li>
+                            <li className="courses-top-bar__selection-item">
+                              مربت سازی بر اساس امتیاز
+                            </li>
+                            <li className="courses-top-bar__selection-item">
+                              مربت سازی بر اساس آخرین
+                            </li>
+                            <li className="courses-top-bar__selection-item">
+                              مربت سازی بر اساس ارزان ترین
+                            </li>
+                            <li className="courses-top-bar__selection-item">
+                              مربت سازی بر اساس گران ترین
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+
+                      <div className="courses-top-bar__left">
+                        <form action="#" className="courses-top-bar__form">
+                          <input
+                            type="text"
+                            className="courses-top-bar__input"
+                            placeholder="جستجوی دوره ..."
+                          />
+                          <i className="fas fa-search courses-top-bar__search-icon"></i>
+                        </form>
+                      </div>
+                    </div>
+                    {courses.map((course) => (
+                      <CourseBox {...course} />
+                    ))}
+                    <Pagination />
+                  </>
+                ) : (
+                  <div className="alert alert-warning">
+                    هنوز هیچ دوره ای برای این دسته بندی وجود ندارد
+                  </div>
+                )}
               </div>
             </div>
           </div>
-
-          <Pagination />
         </div>
       </section>
 
